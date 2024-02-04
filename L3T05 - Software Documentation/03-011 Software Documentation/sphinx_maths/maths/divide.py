4aeeeb9ca817187aaf07dc1b94e72f01e0abbfe6
negin-mgdm <negin.mgdm@gmail.com>
2024-02-04 22:28:32 +0000

    migrating coursework level 3 to git

---
+++ L3T05 - Software Documentation/03-011 Software Documentation/sphinx_maths/maths/divide.py
@@ -0,0 +1,12 @@
+def divide_nums(num1, num2):
+    """This method will be used to divide two numbers
+
+        :param int num1: The first number
+        :param int num2: The second number
+
+        :returns: The division of two numbers
+
+        :rtype: int
+    """
+    answer = num1 / num2
+    return answer
